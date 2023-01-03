c2c12c3efb6a5ae912a5706596ae3cb8e3eb464a
Vasilii Tolstoi <eternalwanderer13@yahoo.com>
2023-01-04 01:43:23 +0600

    created password setter

--- registration.py
+++ registration.py
@@ -5,6 +5,14 @@ class Registration:
         self.login = login
         self.password = password
 
+    @login.setter
+    def login(self, new_login):
+        if '@' not in new_login:
+            raise ValueError("Логин должен содержать один символ '@'")
+        if new_login.find('@') > new_login.find('.'):
+            raise ValueError("Логин должен содержать символ '.'")
+        self.__login = new_login
+
     @property
     def login(self):
         return self.__login
@@ -24,16 +32,19 @@ class Registration:
     @staticmethod
     def is_include_only_latin(value):
         return all(ch in ascii_letters for ch in value if ch.isalpha())
-    
-    @staticmethod
-    def check_password_dictionary(value):
-        with open('easy_passwords.txt', 'r', encoding='utf-8') as f:
-            return value in f.read().split()
 
-    @login.setter
-    def login(self, new_login):
-        if '@' not in new_login:
-            raise ValueError("Логин должен содержать один символ '@'")
-        if new_login.find('@') > new_login.find('.'):
-            raise ValueError("Логин должен содержать символ '.'")
-        self.__login = new_login
+    @password.setter
+    def password(self, value):
+        if not isinstance(value, str):
+            raise TypeError('Пароль должен быть строкой')
+        if not 4 < len(value) < 12:
+            raise ValueError('Пароль должен быть длиннее 4 и меньше 12 символов')
+        if not Registration.is_include_digit(value):
+            raise ValueError('Пароль должен содержать хотя бы одну цифру')
+        if not Registration.is_include_all_register(value):
+            raise ValueError('Пароль должен содержать хотя бы один символ верхнего и нижнего регистра')
+        if not Registration.is_include_only_latin(value):
+            raise ValueError('Пароль должен содержать только латинский алфавит')
+        if Registration.check_password_dictionary(value):
+            raise ValueError('Ваш пароль содержится в списке самых легких')
+        self.__password = value
