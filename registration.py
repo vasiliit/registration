46668924497eee0033b6b6d9ec8a3f5e54f588d4
Vasilii Tolstoi <eternalwanderer13@yahoo.com>
2023-01-04 01:31:11 +0600

    setter of new_login was created

--- registration.py
+++ registration.py
@@ -5,3 +5,11 @@ class Registration:
     @property
     def login(self):
         return self.__login
+
+    @login.setter
+    def login(self, new_login):
+        if '@' not in new_login:
+            raise ValueError("Логин должен содержать один символ '@'")
+        if new_login.find('@') > new_login.find('.'):
+            raise ValueError("Логин должен содержать символ '.'")
+        self.__login = new_login
